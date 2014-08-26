8656d484e2e6888697ff716caa3193b985003b53
Mark Schuegraf <s9mkscue@stud.uni-saarland.de>
2014-08-26 15:23:21 +0200

    Views updated, probleme und unklarheiten updated.

--- SQL/Views.sql
+++ SQL/Views.sql
@@ -1,8 +1,23 @@
 ﻿-- Bietet Kundenname, Modellname, Vor. Lieferdatum, Anzahl, Preis und Auftragsnummer aller Aufträge einer Schnittstelle an
 CREATE OR REPLACE VIEW Kundensicht AS
 	WITH Kundeninfo AS
-			SELECT Name, Aufträge.AID AS Auftrag1 FROM Aufträge JOIN Kunden ON Aufträge.KundenID = Kunden.PID
+		SELECT Name, Aufträge.AID AS Auftrag1 FROM Aufträge JOIN Kunden ON Aufträge.KundenID = Kunden.PID
 	Modellname AS
-			SELECT Bezeichnung, Aufträge.AID AS Auftrag2 FROM Aufträge JOIN Modelle ON Aufträge.Modell_ID = Modelle.Modell_ID WHERE AID = $1
+		SELECT Bezeichnung, Aufträge.AID AS Auftrag2 FROM Aufträge JOIN Modelle ON Aufträge.Modell_ID = Modelle.Modell_ID WHERE AID = $1
 	SELECT Name AS Kunden_Name, Bezeichnung AS Modell, Vorraussichtliches_Lieferdatum, Anzahl, Preis, AID AS Auftragsnummer 
 	FROM (SELECT * FROM Kundeninfo JOIN Modellname ON Auftrag1 = Auftrag2) AS tmp JOIN Aufträge on AID = Auftrag1;
+
+-- Erlaubt einem Hersteller sein Teileangebot einzusehen und ggf. anzupassen
+CREATE OR REPLACE VIEW Herstellerangebot AS
+	WITH Herstellerteile AS
+		SELECT * FROM Hersteller JOIN produzieren ON Hersteller.HID = produzieren.HID
+	SELECT Firmennamen, HID AS Herstelleridentifikationsnummer, TeiletypID, Preis, Zeit FROM Herstellerteile;
+
+-- Zeigt die aktuellen Bestellungen einem Hersteller an (nicht modifizierbar)
+CREATE OR REPLACE VIEW Herstellerbestellungen AS
+	SELECT Firmennamen, HID AS Herstelleridentifikationsnummer, BID AS Bestellungsnummer, TeiletypID, Bezeichnung AS Teilbezeichnung
+	FROM Hersteller JOIN bestellt ON Hersteller.HID = bestellt.HID JOIN Autoteiltypen ON bestellt.TeiletypID = Autoteiltypen.TeiletypID;
+
+CREATE OR REPLACE RULE manufacturerNewPartInInventory AS ON INSERT TO Herstellerangebot 
+WHERE EXISTS SELECT * FROM Autoteiltypen WHERE Autoteiltypen.TeiletypID = NEW.TeiletypID
+DO INSTEAD INSERT INTO produzieren VALUES (NEW.TeiletypID, NEW.Herstelleridentifikationsnummer, NEW.Preis, NEW.Zeit);
