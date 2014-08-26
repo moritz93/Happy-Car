8eb98c42553c5c39ff999cc3e9320d89ed5c509c
Mark Schuegraf <s9mkscue@stud.uni-saarland.de>
2014-08-26 13:06:30 +0200

    Kundenansicht view fertig

---
+++ SQL/Views.sql
@@ -0,0 +1,8 @@
+﻿-- Bietet Kundenname, Modellname, Vor. Lieferdatum, Anzahl, Preis und Auftragsnummer aller Aufträge einer Schnittstelle an
+CREATE OR REPLACE VIEW Kundensicht AS
+	WITH Kundeninfo AS
+			SELECT Name, Aufträge.AID AS Auftrag1 FROM Aufträge JOIN Kunden ON Aufträge.KundenID = Kunden.PID
+	Modellname AS
+			SELECT Bezeichnung, Aufträge.AID AS Auftrag2 FROM Aufträge JOIN Modelle ON Aufträge.Modell_ID = Modelle.Modell_ID WHERE AID = $1
+	SELECT Name AS Kunden_Name, Bezeichnung AS Modell, Vorraussichtliches_Lieferdatum, Anzahl, Preis, AID AS Auftragsnummer 
+	FROM (SELECT * FROM Kundeninfo JOIN Modellname ON Auftrag1 = Auftrag2) AS tmp JOIN Aufträge on AID = Auftrag1;
