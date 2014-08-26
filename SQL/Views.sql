d4a9d67968720eff38ada089d196083b2e70632e
Mark Schuegraf <s9mkscue@stud.uni-saarland.de>
2014-08-26 18:44:53 +0200

    Keine syntaxfehler in views mehr

--- SQL/Views.sql
+++ SQL/Views.sql
@@ -5,10 +5,10 @@
 -- Bietet Kundenname, Modellname, Vor. Lieferdatum, Anzahl, Preis und Auftragsnummer aller Aufträge einer Schnittstelle an
 CREATE OR REPLACE VIEW Kundensicht AS
 	WITH Kundeninfo AS
-		SELECT Name, Aufträge.AID AS Auftrag1 FROM Aufträge JOIN Kunden ON Aufträge.KundenID = Kunden.PID
+		(SELECT Vorname, Nachname, Aufträge.AID AS Auftrag1 FROM Aufträge JOIN Personen ON Aufträge.KundenID = Personen.PID),
 	Modellname AS
-		SELECT Bezeichnung, Aufträge.AID AS Auftrag2 FROM Aufträge JOIN Modelle ON Aufträge.Modell_ID = Modelle.Modell_ID WHERE AID = $1
-	SELECT Name AS Kunden_Name, Bezeichnung AS Modell, Vorraussichtliches_Lieferdatum, Anzahl, Preis, AID AS Auftragsnummer 
+		(SELECT Bezeichnung, Aufträge.AID AS Auftrag2 FROM Aufträge JOIN Modelle ON Aufträge.Modell_ID = Modelle.Modell_ID)
+	SELECT Vorname, Nachname, Bezeichnung AS Modell, Vorraussichtliches_Lieferdatum, Anzahl, Preis, AID AS Auftragsnummer 
 	FROM (SELECT * FROM Kundeninfo JOIN Modellname ON Auftrag1 = Auftrag2) AS tmp JOIN Aufträge on AID = Auftrag1;
 
 
@@ -16,21 +16,28 @@ CREATE OR REPLACE VIEW Kundensicht AS
 -- Erlaubt einem Hersteller sein Teileangebot einzusehen und ggf. anzupassen
 CREATE OR REPLACE VIEW Herstellerangebot AS
 	WITH Herstellerteile AS
-		SELECT * FROM Hersteller JOIN produzieren ON Hersteller.HID = produzieren.HID
+		(SELECT Hersteller.HID AS HID, Firmennamen, TeiletypID, Preis, Zeit FROM Hersteller JOIN produzieren ON Hersteller.HID = produzieren.HID)
 	SELECT Firmennamen, HID AS Herstelleridentifikationsnummer, TeiletypID, Preis, Zeit FROM Herstellerteile;
 
-CREATE OR REPLACE RULE manufacturerCommand AS ON INSERT OR UPDATE OR DELETE TO Herstellerangebot
+-- Notwendig da laut Postgres Doku von CREATE RULE:
+-- "There is a catch if you try to use conditional rules for view updates: 
+-- there must be an unconditional INSTEAD rule for each action you wish to allow on the view."
+CREATE OR REPLACE RULE manufacturerCommandI AS ON INSERT TO Herstellerangebot
+DO INSTEAD NOTHING;
+CREATE OR REPLACE RULE manufacturerCommandU AS ON UPDATE TO Herstellerangebot
+DO INSTEAD NOTHING;
+CREATE OR REPLACE RULE manufacturerCommandD AS ON DELETE TO Herstellerangebot
 DO INSTEAD NOTHING;
 
 CREATE OR REPLACE RULE manufacturerNewPartInInventory AS ON INSERT TO Herstellerangebot 
-DO ALSO WHERE EXISTS SELECT * FROM Autoteiltypen WHERE Autoteiltypen.TeiletypID = NEW.TeiletypID
-	INSERT INTO produzieren VALUES (NEW.TeiletypID, NEW.Herstelleridentifikationsnummer, NEW.Preis, NEW.Zeit);
+WHERE EXISTS (SELECT * FROM Autoteiltypen WHERE Autoteiltypen.TeiletypID = NEW.TeiletypID)
+DO INSERT INTO produzieren VALUES (NEW.TeiletypID, NEW.Herstelleridentifikationsnummer, NEW.Preis, NEW.Zeit);
 
 CREATE OR REPLACE RULE manufacturerPartUpdatedInInventory AS ON UPDATE TO Herstellerangebot 
 -- TeiletypIDs dürfen von Herstellern nicht geändert werden.
 -- Möchte ein Hersteller ein Teil komplett durch ein neues ersetzen muss er stattdessen löschen und dann einfügen.
-DO ALSO WHERE NEW.TeiletypID = OLD.TeiletypID UPDATE produzieren
-	SET HID = NEW.Herstelleridentifikationsnummer AND Preis = NEW.Preis AND Zeit = NEW.Zeit
+WHERE NEW.TeiletypID = OLD.TeiletypID DO ALSO UPDATE produzieren
+	SET HID = NEW.Herstelleridentifikationsnummer, Preis = NEW.Preis, Zeit = NEW.Zeit
 	WHERE TeiletypID = OLD.TeiletypID AND HID = OLD.Herstelleridentifikationsnummer;
 
 CREATE OR REPLACE RULE manufacturerDeletePartInInventory AS ON DELETE TO Herstellerangebot
@@ -41,7 +48,8 @@ DO ALSO DELETE FROM produzieren WHERE TeiletypID = OLD.TeiletypID AND HID = OLD.
 
 -- Zeigt die aktuellen Bestellungen einem Hersteller an (nicht modifizierbar)
 CREATE OR REPLACE VIEW Herstellerbestellungen AS
-	SELECT Firmennamen, HID AS Herstelleridentifikationsnummer, BID AS Bestellungsnummer, Bestellungsstatus, TeiletypID, Bezeichnung AS Teilbezeichnung
+	SELECT Firmennamen, Hersteller.HID AS Herstelleridentifikationsnummer, BID AS Bestellungsnummer, Status AS Bestellungsstatus, 
+	bestellt.TeiletypID AS TeiletypID, Bezeichnung AS Teilbezeichnung
 	FROM Hersteller JOIN bestellt ON Hersteller.HID = bestellt.HID JOIN Autoteiltypen ON bestellt.TeiletypID = Autoteiltypen.TeiletypID;
 
 
@@ -50,11 +58,14 @@ CREATE OR REPLACE VIEW Herstellerbestellungen AS
 CREATE OR REPLACE VIEW LieferungScannen AS
 	SELECT BID AS BestellungsID FROM bestellt;
 
-CREATE OR REPLACE RULE scanCommand AS ON INSERT OR UPDATE OR DELETE TO LieferungScannen
+-- Notwendig da laut Postgres Doku von CREATE RULE:
+-- "There is a catch if you try to use conditional rules for view updates: 
+-- there must be an unconditional INSTEAD rule for each action you wish to allow on the view."
+CREATE OR REPLACE RULE scanCommandU AS ON UPDATE TO LieferungScannen
 DO INSTEAD NOTHING;
 
 CREATE OR REPLACE RULE scanIn AS ON UPDATE TO LieferungScannen
-DO ALSO UPDATE bestellt SET Bestellungsstatus = 'ARCHIVIERT' WHERE BID = OLD.BestellungsID;
+DO ALSO UPDATE bestellt SET Status = 'ARCHIVIERT' WHERE BID = OLD.BestellungsID;
 
 -- Personalmanagement -> Sicht die alle Spezialisierungen konsolidiert
 -- Für jedes Archiv eine View -> bestellungen
