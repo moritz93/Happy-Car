62d9a8632c2f70dfac55a3a0c5d4b38779a49c65
Mark Schuegraf <s9mkscue@stud.uni-saarland.de>
2014-08-27 18:31:52 +0200

    Indizes nun ohne Status als Key.

--- SQL/indizes.sql
+++ SQL/indizes.sql
@@ -1,14 +1,14 @@
 ﻿-- Index Autos
-CREATE INDEX IDX_Autos ON Autos (KFZ_ID, Status) WHERE Status = 'ARCHIVIERT';
+CREATE INDEX IDX_Autos ON Autos (KFZ_ID) WHERE Status = 'ARCHIVIERT';
 
 -- Index Aufträge
-CREATE INDEX IDX_Aufträge ON Aufträge (AID, Status) WHERE Lieferdatum IS NOT NULL;
+CREATE INDEX IDX_Aufträge ON Aufträge (AID) WHERE Lieferdatum IS NOT NULL;
 
 -- Index Werksaufträge
-CREATE INDEX IDX_Werksaufträge ON Werksaufträge (WID, AID, Status) WHERE Status = 'ARCHIVIERT';
+CREATE INDEX IDX_Werksaufträge ON Werksaufträge (WID, AID) WHERE Status = 'ARCHIVIERT';
 
 -- Index bestellt
-CREATE INDEX IDX_bestellt ON bestellt (BID, Status) WHERE Status = 'ARCHIVIERT';
+CREATE INDEX IDX_bestellt ON bestellt (BID) WHERE Status = 'ARCHIVIERT';
 
 -- Index liefert
-CREATE INDEX IDX_liefert ON liefert (KFZ_ID, MID, AID, Status) WHERE Lieferdatum IS NOT NULL;
+CREATE INDEX IDX_liefert ON liefert (KFZ_ID, MID, AID) WHERE Lieferdatum IS NOT NULL;
