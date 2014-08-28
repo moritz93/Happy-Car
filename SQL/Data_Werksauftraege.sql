2f920b7386eb1538eb522c2fcb9af1bc2d40fb34
Moritz Beck <mo_1993@web.de>
2014-08-28 17:53:13 +0200

    beispieldaten

--- SQL/Data_Werksauftraege.sql
+++ SQL/Data_Werksauftraege.sql
@@ -1,3 +1,12 @@
-﻿INSERT INTO Werksaufträge VALUES
---	(WID, AID, status)
-	(1, 0, 'WARTEND');
+﻿INSERT INTO Aufträge (Preis, Modell_ID, Anzahl, KundenID, mitarbeiterID) VALUES
+-- (Preis, Modell_ID, Anzahl, KundenID, MitarbeiterID)
+-- Verwaltungsangestellte 3, 8
+-- Kunden 1, 4, 6, 9
+-- Modell_ID 1-5
+	(39998 , 1, 2, 1, 3),
+	(400000, 2, 3, 4. 8),
+	(400000, 3, 1, 6. 3),
+	(400000, 4, 5, 9. 8),
+	(400000, 5, 10, 9. 8);
+	
+	
