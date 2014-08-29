dd8caea6c9efd5bf87eaf67c1c14eb210ea33a34
Moritz Beck <mo_1993@web.de>
2014-08-29 18:10:29 +0200

    viewMo renamed to analyse

---
+++ SQL/Analyse.sql
@@ -0,0 +1,13 @@
+﻿-- Analysefunktionen
+
+--1: Die Anzahl aller bereits verkauften Autos absteigend sortiert.
+SELECT Modelle.Modell_ID, Bezeichnung, sum(archivierteAufträge.Anzahl) AS "bereits verkauft"
+FROM archivierteAufträge
+JOIN Modelle
+ON Modelle.Modell_ID = archivierteAufträge.Modell_ID
+GROUP BY Modelle.Modell_ID
+ORDER BY "bereits verkauft" DESC
+
+
+
+
