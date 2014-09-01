81b88515426dfc393017d2b93974f2b3ac46e04f
Moritz Beck <mo_1993@web.de>
2014-09-01 13:13:11 +0200

    index

--- SQL/indizes.sql
+++ SQL/indizes.sql
@@ -2,7 +2,7 @@
 CREATE INDEX IDX_Autos ON Autos (KFZ_ID) WHERE Status = 'ARCHIVIERT';
 
 -- Index Aufträge
-CREATE INDEX IDX_Aufträge ON Aufträge (AID) WHERE Lieferdatum IS NOT NULL;
+CREATE INDEX IDX_Aufträge ON Aufträge (AID) WHERE Auftragsstatus = 'ARCHIVIERT';
 
 -- Index Werksaufträge
 CREATE INDEX IDX_Werksaufträge ON Werksaufträge (WID, AID) WHERE Status = 'ARCHIVIERT';
